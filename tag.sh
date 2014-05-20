ab0393a57aafd3208ce8d8789ca1e2417cdb97cb
Kevin Chau <kevin@kevinchau.org>
2014-05-20 10:35:12 -0700

    sacto

--- tag.sh
+++ tag.sh
@@ -2,6 +2,6 @@ curl -F 'client_id=8344cc32e0464b3db83b13575741d9cb' \
      -F 'client_secret=d135ec98c4a04df7866e2a6201d7f356' \
      -F 'object=tag' \
      -F 'aspect=media' \
-     -F 'object_id=divshot' \
+     -F 'object_id=sacto' \
      -F 'callback_url=http://whispering-everglades-6142.herokuapp.com/instagram' \
      https://api.instagram.com/v1/subscriptions/
