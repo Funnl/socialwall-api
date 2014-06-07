30c954799f7832bb3e88f3d7fec29674d0768ad5
Kevin Chau <kevin@kevinchau.org>
2014-06-07 08:16:08 -0700

    default tag and true

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var cache = {
 	instance_id : "mvp",
 	settings : {
 		// TODO: turn tag into an array
-		tag: "sactoadvantage",
+		tag: "angelhackbos",
 		eventId: "default-event",
 		startTime : moment()
 	},
@@ -31,8 +31,8 @@ var cache = {
 		twitterSearch: null,
 	},
 	statuses: {
-		twitter_stream : false,
-		instagram_stream: false
+		twitter_stream : true,
+		instagram_stream: true
 	},
 	counts : { // TODO: later
 		twitter: 0,
